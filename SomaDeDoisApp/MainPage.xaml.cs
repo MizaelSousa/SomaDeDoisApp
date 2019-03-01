1f009a1b4537e0732b2ab49321215c5cddf76946
Mizael Sousa <aluno@MacBook-Pro-de-Aluno.local>
2019-02-28 21:33:27 -0400

    Soma de dois numeros

---
+++ SomaDeDoisApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SomaDeDoisApp
+{
+    public partial class MainPage : ContentPage
+    {
+        void Somar(Object sender, EventArgs e){
+            int resultado = Int32.Parse(N1.Text) + Int32.Parse(N2.Text);
+            Result.Text = resultado.ToString();
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
